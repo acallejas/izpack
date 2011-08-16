c6eade43078c4bfb5275b675dc4a8363e6d49d45
augusto callejas <acallejas@appliedminds.com>
2011-08-16 11:47:29 -0700

    Initializing the 'listeners' instance member.
    Initializing the 'listeners' member, since if it is not initialized
    and fillWithDefault() is called, 'listeners' becomes an
    unmodifiable empty list, which becomes a problem when you try to add
    a new listener via add() - the listener never gets added.

--- izpack-api/src/main/java/com/izforge/izpack/api/data/binding/IzpackProjectInstaller.java
+++ izpack-api/src/main/java/com/izforge/izpack/api/data/binding/IzpackProjectInstaller.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class IzpackProjectInstaller implements Serializable
 {
 
-    private List<Listener> listeners;
+    private List<Listener> listeners = new java.util.ArrayList<Listener>();
 
     private List<Panel> panels;
 
